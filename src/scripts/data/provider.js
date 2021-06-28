a974cfd0652f928e0e5ca93f8bd42830c29ae152
Joel Beckum <joelbeckum@gmail.com>
2021-06-28 16:34:29 -0400

    Completed first issue ticket on Giffygram project; implemented fetch call for populating application state

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -3,10 +3,55 @@ const applicationElement = document.querySelector(".giffygram")
 
 
 const applicationState = {
+    users: [],
+    posts: [],
+    likes: [],
+    messages: [],
+    follows: [],
     currentUser: {},
     feed: {
         chosenUser: null,
         displayFavorites: false,
         displayMessages: false
     }
+}
+
+export const fetchExternalData = () => {
+    return Promise.all([
+        fetch(`${apiURL}/users`),
+        fetch(`${apiURL}/posts`),
+        fetch(`${apiURL}/likes`),
+        fetch(`${apiURL}/messages`),
+        fetch(`${apiURL}/follows`)
+    ]).then(responses => {
+        return Promise.all(responses.map(response => {
+            return response.json()
+        }))
+    }).then(externalData => {
+        applicationState.users = externalData[0]
+        applicationState.posts = externalData[1]
+        applicationState.likes = externalData[2]
+        applicationState.messages = externalData[3]
+        applicationState.follows = externalData[4]
+    })
+}
+
+export const getUsers = () => {
+    return applicationState.users.map(user => ({...user}))
+}
+
+export const getPosts = () => {
+    return applicationState.posts.map(post => ({...post}))
+}
+
+export const getLikes = () => {
+    return applicationState.likes.map(like => ({...like}))
+}
+
+export const getMessages = () => {
+    return applicationState.messages.map(message => ({...message}))
+}
+
+export const getFollows = () => {
+    return applicationState.follows.map(follow => ({...follow}))
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,23 @@
 import { GiffyGram } from "./GiffyGram.js"
+import { LoginForm } from "./auth/Login.js"
+import { fetchExternalData } from "./data/provider.js"
 
 const applicationElement = document.querySelector(".giffygram")
 
 export const renderApp = () => {
-    const user = parseInt(localStorage.getItem("gg_user"))
+    fetchExternalData().then( () => {
+        const user = parseInt(localStorage.getItem("gg_user"))
 
     if (user) {
         applicationElement.innerHTML = GiffyGram()
     } else {
         applicationElement.innerHTML = LoginForm()
     }
+    })
 }
+
+renderApp()
+
+document.addEventListener("stateChanged", event => {
+    renderApp()
+})
