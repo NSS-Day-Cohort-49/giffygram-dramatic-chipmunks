95f81450d00639c9deda80871031c42a437fe24d
Peter Racine Jr <peterracinejr@gmail.com>
2021-07-01 11:02:34 -0500

    added user and 'all users' options in user dropdown

--- src/scripts/nav/Footer.js
+++ src/scripts/nav/Footer.js
@@ -1,4 +1,4 @@
-import { applicationState } from "../data/provider.js";
+import { applicationState, getUsers } from "../data/provider.js";
 
 document.addEventListener("change", (event) => {
   if (event.target.id === "showOnlyFavorites") {
@@ -12,19 +12,36 @@ document.addEventListener("change", (event) => {
   }
 });
 
+// document.addEventListener("change", (event) => {
+//   const userDropdown = document.querySelector("#chosenUser")
+//   if (event.target.id === "author_display_dropdown") {
+
+// });
+
 export const Footer = () => {
+  const users = getUsers();
+  let dropdownHTML = users
+    .map((user) => {
+      return `<option value="${user.id}"> ${user.name}</option>`;
+    })
+    .join("");
+
   let checkBox = "";
   if (applicationState.feed.displayFavorites) {
     checkBox = "checked";
   }
   let html = `
-  <div class="footer">Posts since
-  <select name="post_since_dropdown" id="post_since_dropdown"
+  <div class="footer__item">Posts since
+    <select name="post_since_dropdown" id="chosenUser">
+    </select>
   </div>
-  <div class="footer">Posts by user
+  <div class="footer__item">Posts by user
+    <select name="author_display_dropdown" id="author_display_dropdown"> 
+    ${dropdownHTML} <option value="All"> All Users</option>
+    </select>
   </div>
-  <div class="footer">Show only favorites
-  <input type="checkbox" class="footer__item" id="showOnlyFavorites" ${checkBox}>
+  <div class="footer__item">Show only favorites
+    <input type="checkbox" id="showOnlyFavorites" ${checkBox}>
   </div>`;
 
   return html;
